d29cc1e9cebb2b93bf74e8a8263c306182a0346d
beautifulSun <1223819177@qq.com>
2020-10-13 08:44:47 +0800

    report1-Aimee.refine and complete the tex,stroke weight,font color and The background color of the table

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m " refine and complete the tex,stroke weight,font color and The background color of the table"
+git commit -m " report1-Aimee.refine and complete the tex,stroke weight,font color and The background color of the table"
 git push
